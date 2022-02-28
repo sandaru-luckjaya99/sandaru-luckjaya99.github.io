25cbda8e483b2a9e6ff2ffa4fd1ab5e201644cfa
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2022-02-28 18:03:02 +0530

    Updates

---
+++ precache-manifest.f2c12acf9cfc052c4ee679b64635a7af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eefea8dae35d9a87ab0c24ed1402e35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b428f818e60ef0914c7e",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "db0e8395fd9147b6c10f",
+    "url": "/static/js/2.0bbb0463.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0bbb0463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b428f818e60ef0914c7e",
+    "url": "/static/js/main.846b3994.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "61ff378b4b28e75f9b07bc0afa5d8395",
+    "url": "/static/media/ICC.61ff378b.png"
+  },
+  {
+    "revision": "535e9d3d6cb1f18bcfff1144735882ad",
+    "url": "/static/media/IT_graduate.535e9d3d.pdf"
+  },
+  {
+    "revision": "ef3b929c2115d05763aef2d8b190337d",
+    "url": "/static/media/USJ.ef3b929c.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "5f9b9f4d74bfbaf039cbfe4fd34b6c78",
+    "url": "/static/media/profile.5f9b9f4d.jpg"
+  }
+]);
